092dbf652190d177759caf8f2a9ba12aae8fac7a
Kim Rune Møller <kimrune@hotmail.no>
2024-06-23 20:01:06 +0200

    adds readme, installs packages for cron and google apis

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-backend for https://github.com/kimrm/room-status
+# Backend for room-status app
+
+https://github.com/kimrm/room-status
+
+## About
+
+This is a project for a info-screen solution for meeting-, conference-, and class rooms. It's in its first stages of development and not recommended for use yet.
+
+Please contact [me](https://github.com/kimrm) if you have any questions.
---
+++ src/jobs/calendar-sync-job.ts
@@ -0,0 +1,86 @@
+import * as cron from "node-cron";
+import { google } from "googleapis";
+import { JWT } from "google-auth-library";
+import { PrismaClient } from "@prisma/client";
+import { env } from "node:process";
+
+async function fetchEventsForCurrentDay(calendarId: string, auth: any) {
+  try {
+    const calendar = google.calendar({ version: "v3", auth });
+
+    // Get the current date and time
+    const startOfDay = new Date();
+    const endOfDay = new Date();
+
+    // Set the start time to the beginning of the current day
+    startOfDay.setHours(0, 0, 0, 0);
+
+    // Set the end time to the end of the current day
+    endOfDay.setHours(23, 59, 59, 999);
+
+    const events = await calendar.events.list({
+      calendarId,
+      timeMin: startOfDay.toISOString(),
+      timeMax: endOfDay.toISOString(),
+    });
+
+    return events.data.items || [];
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    throw error;
+  }
+}
+
+async function createAuthClient() {
+  const credentials = require(env.GOOGLE_CREDENTIALS_PATH as string);
+
+  const auth = new JWT({
+    email: credentials.client_email,
+    key: credentials.private_key,
+    scopes: ["https://www.googleapis.com/auth/calendar.readonly"],
+  });
+
+  try {
+    await auth.authorize();
+    return auth;
+  } catch (error) {
+    console.error("Authentication failed:", error);
+    throw error;
+  }
+}
+
+cron.schedule("* * * * *", async () => {
+  const calendarId =
+    "ssmt.no_333232333334332d333838@resource.calendar.google.com";
+  const auth = await createAuthClient();
+
+  console.log("Syncing rooms events for today");
+
+  const prisma = new PrismaClient();
+  const rooms = await prisma.room.findMany();
+
+  rooms.forEach(async (room) => {
+    fetchEventsForCurrentDay(room.calendarId, auth)
+      .then((events) => {
+        events.forEach(async (event) => {
+          if (event.status && event.status === "confirmed") {
+            const booking = await prisma.booking.create({
+              data: {
+                roomId: room.id,
+                description: event.summary as string,
+                from: event.start?.dateTime as string | Date,
+                to: event.end?.dateTime as string | Date,
+                eventId: event.id as string,
+              },
+            });
+            console.log("Created booking:", booking);
+          }
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+
+  console.log("Synced rooms events for today");
+});
